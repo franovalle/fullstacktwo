da3ed2f27ca22639627953d7caca865755a3af50
franovalle <francelie.ovalle@gmail.com>
2025-04-23 13:27:34 -0400

    update

--- app/routes.js
+++ app/routes.js
@@ -1,135 +1,178 @@
 const { result } = require("lodash");
 
-module.exports = function(app, passport, db) {
+module.exports = function (app, passport, db) {
 
-// normal routes ===============================================================
+  // normal routes ===============================================================
 
-    // show the home page (will also have our login links)
-    app.get('/', function(req, res) {
-        res.render('index.ejs');
+  // show the home page (will also have our login links)
+  app.get('/', function (req, res) {
+    res.render('index.ejs');
+  });
+
+  // PROFILE SECTION =========================
+  app.get('/profile', isLoggedIn, function (req, res) {
+    db.collection('verses').find().toArray((err, result) => {
+      if (err) return console.log(err)
+      res.render('profile.ejs', {
+        user: req.user,
+        messages: result
+      })
+    })
+  });
+
+  // LOGOUT ==============================
+  app.get('/logout', function (req, res) {
+    req.logout(() => {
+      console.log('User has logged out!')
     });
+    res.redirect('/');
+  });
 
-    // PROFILE SECTION =========================
-    app.get('/profile', isLoggedIn, function(req, res) {
-        db.collection('verses').find().toArray((err, result) => {
-          if (err) return console.log(err)
-          res.render('profile.ejs', {
-            user : req.user,
-            messages: result
-          })
-        })
-    });
+  // message board routes ===============================================================
 
-    // LOGOUT ==============================
-    app.get('/logout', function(req, res) {
-        req.logout(() => {
-          console.log('User has logged out!')
-        });
-        res.redirect('/');
-    });
-
-// message board routes ===============================================================
-
-    app.post('/verses', (req, res) => {
-      db.collection('verses')
+  app.post('/verses', (req, res) => {
+    db.collection('verses')
       .insertOne(req.body)
       .then(result => {
         console.log(result);
         res.redirect('/')
       })
       .catch(error => console.log(error))
-})
-app.get('/', (req, res) => {
-  const cursor =  db.collection('verses')
-  .find()
-  .toArray()
-  .then(result => {
-    res.render('index.ejs', {verses : results})
-    //console.log(results)
   })
-  //console.log(cursor);
-  .catch(error => console.error(error))
-})
-//using https://zellwk.com/blog/crud-express-mongodb/ as a guide for this project
-        
-       
-
-    app.put('/verses', (req, res) => {
-      db.collection('verses')
-      .findOneAndUpdate({day: req.body.day, entry: req.body.entry}, {
-        $set: {
-          thumbUp:req.body.thumbUp + 1
-        }
-      }, {
-        sort: {_id: -1},
-        upsert: true
-      }, (err, result) => {
-        if (err) return res.send(err)
-        res.send(result)
+  app.get('/', (req, res) => {
+    const cursor = db.collection('verses')
+      .find()
+      .toArray()
+      .then(result => {
+        res.render('index.ejs', { verses: results })
+        //console.log(results)
       })
-    })
+      //console.log(cursor);
+      .catch(error => console.error(error))
+  })
+  //using https://zellwk.com/blog/crud-express-mongodb/ as a guide for this project
+
+//
+app.put('/verses', (req, res) => {
+  console.log(req.body);
+  db.collection('verses')
+  .findOneAndUpdate(
+    { name: 'Me' },
+    {
+      $set: {
+        name: req.body.name,
+        entry: req.body.entry
+
+      },
+    },
+    {
+      upsert: true,
+    }
+  )
+  .then(result => {
+    console.log(result)
+    res.json('Success')
+
+  })
+  .catch(error => console.log(error))
+
+
+})
+//
+
+    /* }) following along with the reading 
+       db.collection('verses')
+       .findOneAndUpdate(
+         {name : 'Me' }
+         {
+           $set: {
+             name: req.body.name, 
+             entry: req.body.entry
+           },
+           {
+             upsert: true,
+           }
+         )
+         .then(result =>{
+           res.json('Sucess')
+ 
+         })
+         .catch(error => console.log(error))
+   
+        /* {day: req.body.day, entry: req.body.entry}, {
+         $set: {
+           thumbUp:req.body.thumbUp + 1
+         }
+       }, {
+         sort: {_id: -1},
+         upsert: true
+       }, (err, result) => {
+         if (err) return res.send(err)
+         res.send(result)
+       })
+     })*/
 
     app.delete('/verses', (req, res) => {
-      db.collection('verses').findOneAndDelete({day: req.body.day, entry: req.body.entry}, (err, result) => {
+      db.collection('verses').findOneAndDelete({ day: req.body.day, entry: req.body.entry }, (err, result) => {
         if (err) return res.send(500, err)
         res.send('Message deleted!')
       })
     })
 
-// =============================================================================
-// AUTHENTICATE (FIRST LOGIN) ==================================================
-// =============================================================================
+    // =============================================================================
+    // AUTHENTICATE (FIRST LOGIN) ==================================================
+    // =============================================================================
 
     // locally --------------------------------
-        // LOGIN ===============================
-        // show the login form
-        app.get('/login', function(req, res) {
-            res.render('login.ejs', { message: req.flash('loginMessage') });
-        });
-
-        // process the login form
-        app.post('/login', passport.authenticate('local-login', {
-            successRedirect : '/profile', // redirect to the secure profile section
-            failureRedirect : '/login', // redirect back to the signup page if there is an error
-            failureFlash : true // allow flash messages
-        }));
-
-        // SIGNUP =================================
-        // show the signup form
-        app.get('/signup', function(req, res) {
-            res.render('signup.ejs', { message: req.flash('signupMessage') });
-        });
-
-        // process the signup form
-        app.post('/signup', passport.authenticate('local-signup', {
-            successRedirect : '/profile', // redirect to the secure profile section
-            failureRedirect : '/signup', // redirect back to the signup page if there is an error
-            failureFlash : true // allow flash messages
-        }));
-
-// =============================================================================
-// UNLINK ACCOUNTS =============================================================
-// =============================================================================
-// used to unlink accounts. for social accounts, just remove the token
-// for local account, remove email and password
-// user account will stay active in case they want to reconnect in the future
-
-    // local -----------------------------------
-    app.get('/unlink/local', isLoggedIn, function(req, res) {
-        var user            = req.user;
-        user.local.email    = undefined;
-        user.local.password = undefined;
-        user.save(function(err) {
-            res.redirect('/profile');
-        });
+    // LOGIN ===============================
+    // show the login form
+    app.get('/login', function (req, res) {
+      res.render('login.ejs', { message: req.flash('loginMessage') });
     });
 
-};
+    // process the login form
+    app.post('/login', passport.authenticate('local-login', {
+      successRedirect: '/profile', // redirect to the secure profile section
+      failureRedirect: '/login', // redirect back to the signup page if there is an error
+      failureFlash: true // allow flash messages
+    }));
 
-// route middleware to ensure user is logged in
-function isLoggedIn(req, res, next) {
+    // SIGNUP =================================
+    // show the signup form
+    app.get('/signup', function (req, res) {
+      res.render('signup.ejs', { message: req.flash('signupMessage') });
+    });
+
+    // process the signup form
+    app.post('/signup', passport.authenticate('local-signup', {
+      successRedirect: '/profile', // redirect to the secure profile section
+      failureRedirect: '/signup', // redirect back to the signup page if there is an error
+      failureFlash: true // allow flash messages
+    }));
+
+    // =============================================================================
+    // UNLINK ACCOUNTS =============================================================
+    // =============================================================================
+    // used to unlink accounts. for social accounts, just remove the token
+    // for local account, remove email and password
+    // user account will stay active in case they want to reconnect in the future
+
+    // local -----------------------------------
+    app.get('/unlink/local', isLoggedIn, function (req, res) {
+      var user = req.user;
+      user.local.email = undefined;
+      user.local.password = undefined;
+      user.save(function (err) {
+        res.redirect('/profile');
+      });
+    });
+
+  };
+
+  // route middleware to ensure user is logged in
+  function isLoggedIn(req, res, next) {
     if (req.isAuthenticated())
-        return next();
+      return next();
 
     res.redirect('/');
-}
+  }
--- public/main.js
+++ public/main.js
@@ -1,51 +1,42 @@
 //using https://zellwk.com/blog/crud-express-mongodb/ as a guide for this project
 
 const update = document.querySelector('#update-button')
+update.addEventListener('click', _ =>{
+  fetch('/verses', {
+    method : 'put',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      name : 'God',
+      entry : 'No matter how you feel, I am with you and love you'
+    })
+    .then(res => {
+      if (res.ok) return res.json()
+    })
+    .then(response => {
+      window.location.reload(true)
+    })
+
+  })
+})
+
+const deleteButton = document.querySelector('#delete-button')
+
+deleteButton.addEventListener('click', _ => {
+  fetch('/verse', {
+    method: 'delete',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      name: 'me'
+    })
+    
+  })
+  .then(res => {
+    if (res.ok) return res.json()
+  })
+  .then(data => {
+    window.location.reload()
+  })
+
+})
 
 
-
-var thumbUp = document.getElementsByClassName("fa-thumbs-up");
-var trash = document.getElementsByClassName("fa-trash");
-
-Array.from(thumbUp).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const msg = this.parentNode.parentNode.childNodes[3].innerText
-        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
-        fetch('messages', {
-          method: 'put',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            'name': name,
-            'msg': msg,
-            'thumbUp':thumbUp
-          })
-        })
-        .then(response => {
-          if (response.ok) return response.json()
-        })
-        .then(data => {
-          console.log(data)
-          window.location.reload(true)
-        })
-      });
-});
-
-Array.from(trash).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const msg = this.parentNode.parentNode.childNodes[3].innerText
-        fetch('messages', {
-          method: 'delete',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            'name': name,
-            'msg': msg
-          })
-        }).then(function (response) {
-          window.location.reload()
-        })
-      });
-});
